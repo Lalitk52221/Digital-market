ae4863e38f8480b761af64c8290e1c4b7a6d5400
Lalitk52221 <lalitk52221@outlook.com>
2024-07-03 19:47:04 +0530

    .

--- src/components/Global/EmptyCart.jsx
+++ src/components/Global/EmptyCart.jsx
@@ -4,7 +4,7 @@ const EmptyCart = () => {
   return (
     <>
         <div className='relative mb-4 h-60 w-60 text-muted-foreground'>
-            <img src="/hippo-empty-cart.png" alt="Empty cart"
+            <img src="./hippo-empty-cart.png" alt="Empty cart"
             className='absolute inset-0 w-full h-full object-contain' />
         </div>
             <div className='text-xl font-semibold'> Your cart is empty </div>
